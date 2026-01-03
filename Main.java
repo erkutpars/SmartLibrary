f0610d7f331c0acd06a1668a4aa10530b2f49348
Erkut Pars <69302002+erkutpars@users.noreply.github.com>
2026-01-03 19:44:27 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import database.Database;
-
-public class Main {
-    public static void main(String[] args) {
-        Database.createTables();
-        System.out.println("SmartLibrary çalışıyor.");
-    }
-}

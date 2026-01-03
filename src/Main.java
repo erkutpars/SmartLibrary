440e10c493e5f7436340dd35666011d43bc35cdf
Erkut Pars <69302002+erkutpars@users.noreply.github.com>
2026-01-03 19:44:13 +0300

    Add main class to initialize SmartLibrary system

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import database.Database;
+
+public class Main {
+    public static void main(String[] args) {
+        Database.createTables();
+        System.out.println("SmartLibrary sistemi çalışıyor.");
+    }
+}

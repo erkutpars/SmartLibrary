e0dd3ec962ea2ec7b3799d0d4f103e0f6f9d4889
Erkut Pars <69302002+erkutpars@users.noreply.github.com>
2026-01-03 19:41:58 +0300

    Create Loan.java

---
+++ src/models/Loan.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Loan {
+
+    private int id;
+    private int bookId;
+    private int studentId;
+    private String dateBorrowed;
+    private String dateReturned;
+
+    public Loan() {}
+
+    public Loan(int id, int bookId, int studentId, String dateBorrowed, String dateReturned) {
+        this.id = id;
+        this.bookId = bookId;
+        this.studentId = studentId;
+        this.dateBorrowed = dateBorrowed;
+        this.dateReturned = dateReturned;
+    }
+
+    public Loan(int bookId, int studentId, String dateBorrowed) {
+        this.bookId = bookId;
+        this.studentId = studentId;
+        this.dateBorrowed = dateBorrowed;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getBookId() { return bookId; }
+    public void setBookId(int bookId) { this.bookId = bookId; }
+
+    public int getStudentId() { return studentId; }
+    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+    public String getDateBorrowed() { return dateBorrowed; }
+    public void setDateBorrowed(String dateBorrowed) { this.dateBorrowed = dateBorrowed; }
+
+    public String getDateReturned() { return dateReturned; }
+    public void setDateReturned(String dateReturned) { this.dateReturned = dateReturned; }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "[LoanID:%d] BookID:%d StudentID:%d Borrowed:%s Returned:%s",
+            id, bookId, studentId, dateBorrowed,
+            (dateReturned == null ? "-" : dateReturned)
+        );
+    }
+}
